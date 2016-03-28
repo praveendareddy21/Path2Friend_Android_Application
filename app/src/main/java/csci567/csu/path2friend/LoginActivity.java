0aeaf8637aa872257937bee2d81571fe25275a17
Mohit Athwani <mathwani@mail.csuchico.edu>
2016-03-27 20:15:24 -0700

    Added code for Google login need to fix the workflow

--- app/src/main/java/csci567/csu/path2friend/LoginActivity.java
+++ app/src/main/java/csci567/csu/path2friend/LoginActivity.java
@@ -1,19 +1,27 @@
 package csci567.csu.path2friend;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Request;
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Response;
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResponseListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.security.api.AuthorizationManager;
+import com.ibm.mobilefirstplatform.clientsdk.android.security.googleauthentication.GoogleAuthenticationManager;
 
 import org.json.JSONObject;
 
@@ -22,6 +30,7 @@ import java.net.MalformedURLException;
 public class LoginActivity extends AppCompatActivity {
 
     static String TAG = "Login Activity:";
+    final int MY_PERMISSIONS_REQUEST_GET_ACCOUNTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,28 +52,89 @@ public class LoginActivity extends AppCompatActivity {
         try {
             BMSClient.getInstance().initialize(getApplicationContext(),
                     "https://path2friend.mybluemix.net", "dfb91080-472f-450d-b40a-a9cfeb0bc3b7");
-            Request request = new Request("/protected", Request.GET);
 
-            request.send(this, new ResponseListener() {
-                @Override
-                public void onSuccess(Response response) {
-                    Log.d(TAG, "onSuccess :: " + response.getResponseText());
-                }
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.GET_ACCOUNTS)
+                    != PackageManager.PERMISSION_GRANTED) {
 
-                @Override
-                public void onFailure(Response response, Throwable t, JSONObject extendedInfo) {
-                    if (null != t) {
-                        Log.d(TAG, "onFailure :: " + t.getMessage());
-                    } else if (null != extendedInfo) {
-                        Log.d(TAG, "onFailure :: " + extendedInfo.toString());
-                    } else {
-                        Log.d(TAG, "onFailure :: " + response.getResponseText());
-                    }
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.GET_ACCOUNTS)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.GET_ACCOUNTS},
+                            MY_PERMISSIONS_REQUEST_GET_ACCOUNTS);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
                 }
-            });
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_GET_ACCOUNTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    GoogleAuthenticationManager.getInstance().register(this);
+                    Request request = new Request("/protected", Request.GET);
+
+                    request.send(this, new ResponseListener() {
+                        @Override
+                        public void onSuccess(Response response) {
+                            Log.d(TAG, "onSuccess :: " + response.getResponseText());
+                            Toast.makeText(LoginActivity.this, AuthorizationManager.getInstance().getUserIdentity().toString(), Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, AuthorizationManager.getInstance().getUserIdentity().toString());
+                        }
+
+                        @Override
+                        public void onFailure(Response response, Throwable t, JSONObject extendedInfo) {
+                            if (null != t) {
+                                Log.d(TAG, "onFailure :: " + t.getMessage());
+                            } else if (null != extendedInfo) {
+                                Log.d(TAG, "onFailure :: " + extendedInfo.toString());
+                            } else {
+                                Log.d(TAG, "onFailure :: " + response.getResponseText());
+                            }
+                        }
+                    });
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        GoogleAuthenticationManager.getInstance()
+                .onActivityResultCalled(requestCode, resultCode, data);
     }
 
     @Override
