ffea0cbce4de17837a76b53e9b189a77a4eade59
praveendareddy21 <praveendareddy@gmail.com>
2016-04-11 16:05:21 -0700

    added permission of location track to Google Maps path activity.java

--- app/src/main/java/csci567/csu/path2friend/googlemapspath/GoogleMapsPathActivity.java
+++ app/src/main/java/csci567/csu/path2friend/googlemapspath/GoogleMapsPathActivity.java
@@ -12,14 +12,26 @@ package csci567.csu.path2friend.googlemapspath;
 
     import org.json.JSONObject;
 
+    import android.Manifest;
+    import android.app.AlertDialog;
+    import android.content.Context;
+    import android.content.DialogInterface;
+    import android.content.Intent;
+    import android.content.pm.PackageManager;
     import android.graphics.Color;
+    import android.location.Location;
+    import android.location.LocationListener;
+    import android.location.LocationManager;
     import android.os.AsyncTask;
     import android.os.Bundle;
     import android.support.design.widget.FloatingActionButton;
     import android.support.design.widget.Snackbar;
+    import android.support.v4.app.ActivityCompat;
     import android.support.v4.app.FragmentActivity;
+    import android.support.v4.content.ContextCompat;
     import android.util.Log;
     import android.view.View;
+    import android.widget.Toast;
 
     import csci567.csu.path2friend.R;
     import csci567.csu.path2friend.database.Model;
@@ -35,6 +47,7 @@ public class GoogleMapsPathActivity extends FragmentActivity {
 
     private  LatLng origin = null;
     private LatLng destination = null;
+    final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
     private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543, -73.998585);
     private static final LatLng WALL_STREET = new LatLng(40.7064, -74.0094);
@@ -79,6 +92,8 @@ public class GoogleMapsPathActivity extends FragmentActivity {
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination,
                 13));
         addMarkers();
+
+        handlePermissionForLocation();
 
         FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab);
         fab1.setOnClickListener(new View.OnClickListener() {
@@ -242,6 +257,125 @@ public class GoogleMapsPathActivity extends FragmentActivity {
 
     }
 
+
+    void handlePermissionForLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Toast.makeText(this, "Please give us permission to access your location. We need this to allow you and your friend to track each other", Toast.LENGTH_LONG).show();
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+
+                //write code to fetch location here.
+            }
+        }
+
+        else if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startLocationTracking();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    startLocationTracking();
+
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    void startLocationTracking() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+        }
+
+        LocationListener locationListener = new MyLocationListener();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 500, 10, locationListener);
+    }
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            //editLocation.setText("");
+            //pb.setVisibility(View.INVISIBLE);
+            Toast.makeText(getBaseContext(),
+                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " + loc.getLongitude();
+            Log.v(TAG, longitude);
+            String latitude = "Latitude: " + loc.getLatitude();
+            Log.v(TAG, latitude);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    }
 
 
 
